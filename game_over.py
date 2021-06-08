75bd83fa7dad4b2fb53c428a9d1e7d0e670a9219
Urszula Spik <262335@student.pwr.edu.pl>
2021-06-08 17:10:26 +0200

    Zmiana ekranu końcowego gry

---
+++ game_over.py
@@ -0,0 +1,156 @@
+import arcade
+from arcade.gui import UIManager
+import buttons
+from settings import *
+import results_read
+
+class GameOverView(arcade.View):
+    """ View to show when game is over """
+
+    def __init__(self, score, user):
+        """ This is run once when we switch to this view """
+        super().__init__()
+        self.texture = arcade.load_texture(BACKGROUNDS["game_over"])
+        self.ui_manager = UIManager()
+        self.score = score
+        self.user = user
+
+    def on_draw(self):
+        """ Draw this view """
+        arcade.start_render()
+        self.texture.draw_sized(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+                                SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    def on_hide_view(self):
+        self.ui_manager.unregister_handlers()
+    
+    def on_show_view(self):
+        self.setup()
+
+    def setup(self):
+        """ Set up this view. """
+        self.ui_manager.purge_ui_elements()
+        results_read.today_points_write(RESULTS["level1"], self.score, self.user.text)
+        y_slot = self.window.height // 6
+
+        button = buttons.ExitButton(
+            'Exit',
+            center_x=self.window.width // 2,
+            center_y=y_slot * 1,
+            width=250,
+        )
+        button.set_style_attrs(         
+            bg_color_hover=(135, 21, 25),
+            bg_color_press=(122, 21, 24),
+        )
+        self.ui_manager.add_ui_element(button)
+
+        button = buttons.MenuButton(
+            'Menu',
+            center_x=self.window.width // 2,
+            center_y=y_slot * 2,
+            width=250,
+        )
+        button.set_style_attrs(         
+            bg_color_hover=(226, 99, 1),
+            bg_color_press=(192, 85, 3),
+        )
+        self.ui_manager.add_ui_element(button)
+
+        button = buttons.Level1Button( "Play again",
+            center_x=self.window.width // 2,
+            center_y=y_slot * 3,
+            width=250,
+            user=self.user
+        )
+        button.set_style_attrs(         
+            bg_color_hover=(226, 99, 1),
+            bg_color_press=(192, 85, 3),
+        )
+        self.ui_manager.add_ui_element(button)
+
+        score_label = arcade.gui.UILabel(
+                f'Your score: {self.score}',
+                center_x=self.window.width // 2,
+                center_y=y_slot * 4
+        )
+        score_label.set_style_attrs(
+            font_color=(255,204,51),
+            font_color_hover = (255, 228, 14)
+        )
+        self.ui_manager.add_ui_element(score_label)
+
+class GameOverView2(arcade.View):
+    """ View to show when game is over """
+
+    def __init__(self, score, user):
+        """ This is run once when we switch to this view """
+        super().__init__()
+        self.texture = arcade.load_texture(BACKGROUNDS["game_over"])
+        self.ui_manager = UIManager()
+        self.score = score
+        self.user = user
+
+    def on_draw(self):
+        """ Draw this view """
+        arcade.start_render()
+        self.texture.draw_sized(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+                                SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    def on_hide_view(self):
+        self.ui_manager.unregister_handlers()
+    
+    def on_show_view(self):
+        self.setup()
+
+    def setup(self):
+        """ Set up this view. """
+        self.ui_manager.purge_ui_elements()
+        results_read.today_points_write(RESULTS["level2"], self.score, self.user.text)
+        y_slot = self.window.height // 6
+        button = buttons.ExitButton(
+            'Exit',
+            center_x=self.window.width // 2,
+            center_y=y_slot * 1,
+            width=250,
+        )
+        button.set_style_attrs(         
+            bg_color_hover=(135, 21, 25),
+            bg_color_press=(122, 21, 24),
+        )
+        self.ui_manager.add_ui_element(button)
+
+        button = buttons.MenuButton(
+            'Menu',
+            center_x=self.window.width // 2,
+            center_y=y_slot * 2,
+            width=250,
+        )
+        button.set_style_attrs(         
+            bg_color_hover=(226, 99, 1),
+            bg_color_press=(192, 85, 3),
+        )
+        self.ui_manager.add_ui_element(button)
+
+        button = buttons.Level2Button( "Play again",
+            center_x=self.window.width // 2,
+            center_y=y_slot * 3,
+            width=250,
+            user=self.user
+        )
+        button.set_style_attrs(         
+            bg_color_hover=(226, 99, 1),
+            bg_color_press=(192, 85, 3),
+        )
+        self.ui_manager.add_ui_element(button)
+
+        score_label = arcade.gui.UILabel(
+                f'Your score: {self.score}',
+                center_x=self.window.width // 2,
+                center_y=y_slot * 4
+        )
+        score_label.set_style_attrs(
+            font_color=(255,204,51),
+            font_color_hover = (255, 228, 14)
+        )
+        self.ui_manager.add_ui_element(score_label)
